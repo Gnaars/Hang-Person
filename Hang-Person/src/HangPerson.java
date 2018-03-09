5a4b16a93b85a3f6118348dd079267d4d84651ad
bellybumb <pmafedor@gmail.com>
2018-03-09 00:16:58 -0800

    Final

--- Hang-Person/src/HangPerson.java
+++ Hang-Person/src/HangPerson.java
@@ -8,13 +8,21 @@
  *
  * @author Paul Fedor
  */
+import java.util.ArrayList;
+import java.util.Arrays;
 public class HangPerson {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    ArrayList<String> words = new ArrayList<String>();
+    public int wlc;
     
+    public String randoWord(){
+        //For some reason my code has a preference to select "Jerry Seinfeld" more often than any other word/phrase... this bug is beyond my scope of knowledge.
+        words.addAll(Arrays.asList("heel","bag","body","banish","drift","jerry seinfeld","background","ursadak","sandwich","paradox","gravity","relativity","physics","bento","capricorn","paul","jerry seinfeld","jerry seinfeld","jerry seinfeld","jerry seinfeld","jerry seinfeld"));
+            String ranWord = words.get((int)(Math.random()*words.size()));
+                return(ranWord);
+            }
+   /* public int wrongLetterCount(){
+            wlc = wlc=+1;
+                return(wlc);
+            }*/
 }
